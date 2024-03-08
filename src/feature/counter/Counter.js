13aba709cd6de27ee620b53c534fb9b528b36c6f
wan <conut22@naver.com>
2024-03-08 22:01:41 +0900

    Add redux & redux toolkit in  in counter component.js component

---
+++ src/feature/counter/Counter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './counterSlice';
+
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment by 5
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Counter from 'feature/counter/Counter';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      {/* <App /> */}
+      <Counter />
+    </Provider>
+  </React.StrictMode>,
 );
